351e9a330324326c3267f25fd06b1a32b2a137d1
Sojith Sunny <sojithsunny29034@gmail.com>
2025-01-05 20:27:21 +0530

    routes

--- studyy/src/App.jsx
+++ studyy/src/App.jsx
@@ -7,7 +7,8 @@ import Login from './pages/Login';
 import TeacherForm from './teacher/teacherForm';
 import { useState, useEffect } from 'react';
 import Admin from './admin/Admin';
-import Community from './pages/Community';
+import Student from './student/Student';
+import Teacher from './teacher/Teacher';
 
 function App() {
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(() => {
@@ -15,10 +16,28 @@ function App() {
     return savedStatus === 'true'; // Convert from string to boolean
   });
 
+  const [isStudentLoggedIn, setIsStudentLoggedIn] = useState(() => {
+    const savedStatus = localStorage.getItem('isStudentLoggedIn');
+    return savedStatus === 'true';
+  });
+
+  const [isTeacherLoggedIn, setIsTeacherLoggedIn] = useState(() => {
+    const savedStatus = localStorage.getItem('isTeacherLoggedIn');
+    return savedStatus === 'true';
+  });
+
   // Save the login status to localStorage when it changes
   useEffect(() => {
     localStorage.setItem('isAdminLoggedIn', isAdminLoggedIn.toString());
   }, [isAdminLoggedIn]);
+
+  useEffect(() => {
+    localStorage.setItem('isStudentLoggedIn', isStudentLoggedIn.toString());
+  }, [isStudentLoggedIn]);
+
+  useEffect(() => {
+    localStorage.setItem('isTeacherLoggedIn', isTeacherLoggedIn.toString());
+  }, [isTeacherLoggedIn]);
 
   return (
     <div style={{ backgroundColor: '#222', minHeight: '100vh' }}>
@@ -27,18 +46,34 @@ function App() {
         <Routes>
           <Route path="/" element={<Quiz />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/form/:id" element={<TeacherForm/>} />
-          <Route path="/community" element={<Community/>} />
+          <Route path="/form/:id" element={<TeacherForm />} />
           
           <Route
             path="/login"
-            element={<Login onLogin={() => setIsAdminLoggedIn(true)} />}
+            element={
+              <Login
+                onAdminLogin={() => setIsAdminLoggedIn(true)}
+                onStudentLogin={() => setIsStudentLoggedIn(true)}
+                onTeacherLogin={() => setIsTeacherLoggedIn(true)}
+              />
+            }
           />
           <Route
             path="/admin/*"
             element={isAdminLoggedIn ? <Admin /> : <Navigate to="/login" />}
           />
-
+          <Route
+            path="/student/*"
+            element={
+              isStudentLoggedIn ? <Student /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/teacher/*"
+            element={
+              isTeacherLoggedIn ? <Teacher /> : <Navigate to="/login" />
+            }
+          />
         </Routes>
       </Router>
     </div>
---
+++ studyy/src/student/Student.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Student = () => {
+  return (
+    <div style={{ color:'#ccc' }}>
+      Student
+    </div>
+  )
+}
+
+export default Student
